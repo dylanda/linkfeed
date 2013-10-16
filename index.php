d0a9ffa9258d7b1579dc572a36c6281801d60f74
dylanda <dylan.daconceicao@gmail.com>
2013-10-16 23:32:26 +0200

    init

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="utf-8">
+    <title>Link &middot; Feed</title>
+
+</head>
+
+<body>
+
+<h1>Link Feed</h1>
+<?php
+echo "test";
+?>
+</body>
+
+</html>
