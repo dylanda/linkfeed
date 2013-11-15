89192517be007644e48e5596ba342890aab29b91
loicszym <szymanskiloic@gmx.fr>
2013-11-15 21:59:19 +0100

    ajout sessions

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var database="linkfeed";
 var databaseUrl = host+":"+port+"/"+database;
 var collections = ["users","liens"];
 var db = require("mongojs").connect(databaseUrl, collections);
-var app=express();
+var app = express();
 
 
 
@@ -17,11 +17,25 @@ app.configure(function(){
 		app.set('view engine','jade');
 		app.use(express.bodyParser());
 		app.use(express.static(__dirname + '/public'));
+		app.use(express.cookieParser());
+        app.use(express.session({ secret: "mysecret" }));
 });
 
+
+//fonction callback
+function requiresLogin(req,res,next) {
+    console.log("on test si la session existe");
+    if(req.session.user){
+        next();
+    } else {
+        res.render('index.jade');
+    }
+};
+
+
 //index
-app.get('/',function(request,response){
-		response.render("index.jade");
+app.get('/',requiresLogin, function(request,response){
+		response.render("profil.jade",request.session);
 });
 
 //users
@@ -31,30 +45,42 @@ app.post('/user/login', function(request,response){
 					response.render('index.jade',{messageError:"Mauvais login ou mot de passe"});
 				}
 				else{
-					response.render('profil.jade',request.body);
+					request.session.user = request.body.username;
+					response.render('profil.jade',request.session);
 				}
 		});
 });
+
 			
 app.post('/user/new', function(request,response){
 		db.users.find({_id:request.body.username},function(error,user){
 				if(user.length==0){
 					db.users.insert({_id:request.body.username, mdp:request.body.mdp, email:request.body.email});
 					console.log("Nouvel utilisateur enregistré");
-					response.render('profil.jade',request.body);
+					request.session.user = request.body.username;
+					response.render('profil.jade',request.session);
 				}
 				else{
-					//response.alert('L\'utilisateur existe déjà');
 					response.render('index.jade',{messageError2:"L'utilisateur existe déjà"});
 				}
 		});
 });
 
+
 //liens
 app.post('/lien/new',function(request,response){
-		db.liens.insert({url:request.body.url, description:request.body.description, tags:request.body.tags});
+		db.liens.insert({_id:request.session.user, url:request.body.url, description:request.body.description, tags:request.body.tags});
 		console.log("Nouveau lien enregistré");
-		response.render('profil.jade',request.body);
+		response.render('profil.jade',request.session);
+});
+
+
+//logout
+app.get("/user/logout", function(request, response){
+        if(request.session.user){
+                request.session.user=undefined;
+        }
+        response.render('index.jade');
 });
 
 app.listen(8080);
